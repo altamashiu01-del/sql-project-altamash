97ca46d1db9c342aff3b945b8c9f7dc270bf3847
Altamash <altamashiu01@gmail.com>
2026-04-26 19:27:05 +0530

    enter

---
+++ postgres.session.sql
@@ -0,0 +1,11 @@
+INSERT INTO user_events (
+    event_id,
+    user_id,
+    event_type,
+    event_date,
+    product_id,
+    amount,
+    traffic_source
+)
+VALUES
+(8490, 5024, 'page_view', '58:24.5', 205, NULL, 'social');
